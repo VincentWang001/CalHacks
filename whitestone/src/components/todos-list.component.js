3ee3418df7d9b55bdf60e4e01353545b3ef095fa
VincentWang001 <vwang112@gmail.com>
2021-10-23 12:24:01 -0700

    more changes

--- whitestone/src/components/todos-list.component.js
+++ whitestone/src/components/todos-list.component.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import pepeSad from "../pepecry.png";
 
 export default class TodosList extends Component {
     render() {
         return (
             <div>
                 <p>Welcome to Todos List Component!!</p>
+                <img src={pepeSad} width="300" height="300"/>
             </div>
         )
     }
