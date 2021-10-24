889b3f4d026a692c475a6cc8c4f448e409cb87c0
Roshan Thomas <roshanthomas@MacBook-Pro.local>
2021-10-23 19:04:16 -0700

    pull stuff

--- whitestone/src/components/stockInfo.js
+++ whitestone/src/components/stockInfo.js
@@ -6,23 +6,70 @@ class Stock extends React.Component {
         super(props);
 
         this.onChangeStock = this.onChangeStock.bind(this);
+        this.onStockSubmit = this.onStockSubmit.bind(this);
 
         this.state = {
             stockChartXValues: [],
             stockChartYValues: [],
-            stock: ''
+            stock: 'TSLA',
+            isSubmitted: false
         }
     }
 
-
+    componentDidMount() {
+        this.fetchStock();
+    }
 
     onChangeStock(e) {
         this.setState({
             stock: e.target.value
         });
+        console.log(this.state.stock)
+        console.log('TSLA' == this.state.stock)
     }
 
+    onStockSubmit() {
+        this.setState({
+            isSubmitted: true
+        })
 
+    }
+
+    fetchStock() {
+        if (this.state.isSubmitted) {
+            const pointerToThis = this;
+            console.log(pointerToThis);
+            const API_KEY = 'KRWWGZHCPVK2G29U';
+            let StockSymbol = 'TSLA';
+            console.log(StockSymbol == this.state.stock)
+            let API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${this.state.stock}&outputsize=compact&apikey=${API_KEY}`;
+            let stockChartXValuesFunction = [];
+            let stockChartYValuesFunction = [];
+
+            fetch(API_Call)
+                .then(
+                    function (response) {
+                        return response.json();
+                    }
+                )
+                .then(
+                    function (data) {
+                        console.log(data);
+
+                        for (var key in data['Time Series (Daily)']) {
+                            stockChartXValuesFunction.push(key);
+                            stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
+                        }
+
+                        // console.log(stockChartXValuesFunction);
+                        pointerToThis.setState({
+                            stockChartXValues: stockChartXValuesFunction,
+                            stockChartYValues: stockChartYValuesFunction
+                        });
+                    }
+                )
+        }
+    }
 
     render() {
         return (
@@ -32,8 +79,20 @@ class Stock extends React.Component {
                     value={this.state.stock}
                     onChange={this.onChangeStock}
                 />
+                <input type="submit" value="Submit" onSubmit={this.onSubmitStock} />
                 <p>{this.state.stock}</p>
-
+                <Plot
+                    data={[
+                        {
+                            x: this.state.stockChartXValues,
+                            y: this.state.stockChartYValues,
+                            type: 'scatter',
+                            mode: 'lines+markers',
+                            marker: { color: 'red' },
+                        }
+                    ]}
+                    layout={{ width: 720, height: 440, title: 'A Fancy Plot' }}
+                />
             </div>
         )
     }
